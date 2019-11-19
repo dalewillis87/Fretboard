49f2fbcdf1558cbf4da369c88e03d7ba5e73b890
Dale <dale.willis87@outlook.com>
2019-11-19 12:54:07 +0000

    Code has been tidied of redundancies
    To do: populate tuning array dynamically.

--- index.php
+++ index.php
@@ -3,6 +3,11 @@ $frets = ['Nut', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X', 'X
 $strings = ['E', 'B', 'G', 'D', 'A', 'E'];
 $tempered = ['A', 'A#/Bb', 'B', 'C', 'C#/Db', 'D', 'D#/Eb', 'E', 'F', 'F#/Gb', 'G', 'G#/Ab'];
 $tuning = array(32, 27, 23, 18, 13, 8);
+$num_string = 6;
+
+
+$tempered = new ArrayIterator($tempered);
+$tempered = new InfiniteIterator($tempered);
 
 ?>
 <!DOCTYPE html>
@@ -15,36 +20,25 @@ $tuning = array(32, 27, 23, 18, 13, 8);
 
   <body>
 
+	<h1> Standard Guitar Tuning </h1>
+
     <table width="60%" border="1px">
 		<tr>
 			<?php $num_items = count($frets); for($i = 0; $i < $num_items; $i++) {echo "<th>$frets[$i]</th>";}?>
 		</tr>
 		<?php
-			$count_items = count($strings);
-			$num_string = 6;
-		
-			$tempered = new ArrayIterator($tempered);
-			$tempered = new InfiniteIterator($tempered);
-			$i = 0;
-			
-			foreach ($tuning as $initial) {
-				echo '<tr class="string';
-				echo $num_string;
-				echo '"><td class="nut">';
-				echo $strings[$i];
-				echo '</td>';
-				
-				
-				foreach (new LimitIterator($tempered, $initial, (count($frets) -1)) as $note) {
-					echo '<td>';
-					echo $note;
-					echo '</td>';
-				}				
-				echo '</tr>';
-				$num_string--;
-				$i++;
+		$i = 0;
+
+		foreach ($tuning as $initial) {
+			echo '<tr class="string', $num_string, '"><td class="nut,', $strings[$i], '">', $strings[$i], '</td>';
+
+			foreach (new LimitIterator($tempered, $initial, (count($frets) -1)) as $note) {
+				echo '<td class="', $note, '">', $note, '</td>';
 			}
+			echo '</tr>';
+			$num_string--;
+			$i++;
+		}
 		?>
 	</table>
-
   </body>
